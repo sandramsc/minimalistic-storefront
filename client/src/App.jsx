0f6ae6ce2ac6f380e2a364934905c192582b92fe
sandramsc <sajustsmile@gmail.com>
2022-05-07 03:35:23 +0200

    removed home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,15 @@
-/* Designed & coded by Sandra Ashipala <https://github.com/sandramsc> */
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-//import ProductList from './components/ProductList';
-import Nav from "./components/Navbar/Nav";
-import './App.css'
+import React from "react";
+import { BrowserRouter,  Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
 
-
-export class Main extends Component {
-  constructor(props){
-    super(props);
-    //enbales currency change in respective component
-    this.state = {
-      currentCurrency: "",
-      currentCategory: "",
-    }
-  }
-
-  // currency label
-  setCurrency = (currentCurrency) => {
-    this.setState((prev) => {
-      return { ...prev, currentCurrency};
-    });
-  };
-
-  // category
-  setCategory = (currentCategory) => {
-    this.setState((prev) => {
-      return { ...prev, currentCategory };
-    });
-  }
+function App() {
   
-  render() {
-    const {  currentCurrency, currentCategory } = this.state;
-
-    return(
-      <Router>
-          <Nav 
-            currentCurrency={this.state.currentCurrency}
-            setCurrency={this.setCurrency}
-            setCategory={this.setCategory}
-            currentCategory={this.currentCategory}
-          />
-      </Router>
-    )
+    return (
+      <>
+      <Main/>
+      </>
+    );
   }
-}
-export default Main;
+
+
+export default App;
---
+++ client/src/pages/Main.jsx
@@ -0,0 +1,48 @@
+/* Designed & coded by Sandra Ashipala <https://github.com/sandramsc> */
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import ProductList from '../components/ProductList';
+import Nav from "../components/Navbar/Nav";
+import '../App.css'
+
+
+export class Main extends Component {
+  constructor(props){
+    super(props);
+    //enbales currency change in respective component
+    this.state = {
+      currentCurrency: "",
+      currentCategory: "",
+    }
+  }
+
+  // currency label
+  setCurrency = (currentCurrency) => {
+    this.setState((prev) => {
+      return { ...prev, currentCurrency};
+    });
+  };
+
+  // category
+  setCategory = (currentCategory) => {
+    this.setState((prev) => {
+      return { ...prev, currentCategory };
+    });
+  }
+  
+  render() {
+    const {  currentCurrency, currentCategory } = this.state;
+
+    return(
+      <Router>
+          <Nav 
+            currentCurrency={this.state.currentCurrency}
+            setCurrency={this.setCurrency}
+            setCategory={this.setCategory}
+            currentCategory={this.currentCategory}
+          />
+      </Router>
+    )
+  }
+}
+export default Main;
